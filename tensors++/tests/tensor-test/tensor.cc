d6f48afddd2e5f55b86479323acc0051fce01fb7
coder3101 <ashar786khan@gmail.com>
2018-08-08 00:19:57 +0530

    Using eigen over tensor++ core

--- tensors++/tests/tensor-test/tensor.cc
+++ tensors++/tests/tensor-test/tensor.cc
@@ -14,17 +14,16 @@
  *   limitations under the License.
  */
 
-//#include "tensors++/core/tensor.hpp"
+#include "tensors++/core/tensor.hpp"
 #include <gtest/gtest.h>
 
-TEST(FOO, BAR){
-    ASSERT_EQ(5,5);
-}
+TEST(FOO, BAR) {}
 
-int main(int argc, char **argv){
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
 // TO RUN A TEST : g++ -pthread file.cc -lgtest -lgtest_main
+// alternatively you can use the test  runner executable.
 // Run the output file a.out to see the results of test.
